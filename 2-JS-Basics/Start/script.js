84a8287001317df4b6276b5d22f436848d00c75d
Dave Simonson <dsimo@yahoo.com>
2017-07-06 09:25:40 -0500

    continue

--- 2-JS-Basics/Start/script.js
+++ 2-JS-Basics/Start/script.js
@@ -1,1 +1,17 @@
 console.log('Hello World!');
+
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var favoriteFood = prompt('What is your favorite food?');
+
+console.log(favoriteFood);
